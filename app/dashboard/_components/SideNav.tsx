eda84e36c92bca336e17aa5374d42485d4f566a2
hsahu4012 <hrudayanandasahu3@gmail.com>
2024-07-09 10:16:44 +0530

    Warning Fix

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -46,7 +46,7 @@ const SideNav = () => {
             <div className='mt-3'>
                 {MenuList.map((menu, index) => (
                     <div className={`flex gap-2 mb-2 p-3 
-                    hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${path == menu.path && 'bg-primary text-white'}`}>
+                    hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${path == menu.path && 'bg-primary text-white'}`} key={index}>
                         <menu.icon className='h-6 w-6' />
                         <h2 className='text-lg'>{menu.name}</h2>
                     </div>
