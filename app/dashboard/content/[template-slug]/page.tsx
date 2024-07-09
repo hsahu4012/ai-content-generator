fffbdf8882598895b9a0597565c665cac881cf92
hsahu4012 <hrudayanandasahu3@gmail.com>
2024-07-09 16:08:06 +0530

    Error Fix

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,62 +1,8 @@
-
 import React from 'react'
 
 const CreateNewContent = () => {
     return (
         <div> <h1>CreateNewContent</h1>
- 
-
-"use client"
-import React, { useState } from 'react'
-import FormSection from '../_components/FormSection'
-import OutputSection from '../_components/OutputSection'
-import { TEMPLATE } from '../../_components/TemplateListSection'
-import Templates from '@/app/(data)/Templates'
-import { Button } from '@/components/ui/button'
-import { ArrowLeft } from 'lucide-react'
-import Link from 'next/link'
-import { chatSession } from '@/utils/AiModel'
-
-interface PROPS {
-    params: {
-        'template-slug': string,
-    }
+        </div>)
 }
-
-const CreateNewContent = (props: PROPS) => {
-
-    const selectedTemplate: TEMPLATE | undefined = Templates?.find((item) => item.slug == props.params['template-slug']);
-    const [loading, setLoading] = useState(false);
-    const [aiOutput, setAiOutput] = useState<string>("");
-
-    const GenerateAIContent = async (formData: any) => {
-        setLoading(true);
-        const selectedPrompt = selectedTemplate?.aiPrompt;
-        const FinalAIPrompt = JSON.stringify(formData) + ", " + selectedPrompt;
-        const result = await chatSession.sendMessage(FinalAIPrompt);
-        console.log(result.response.text());
-        setAiOutput(result?.response.text());
-        setLoading(false);
-    }
-
-
-    return (
-        <div className='p-5'>
-            <Link href={'/dashboard'}> <Button ><ArrowLeft />Back</Button></Link>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
-                {/* Form Section  */}
-                <FormSection selectedTemplate={selectedTemplate} userFormInput={(v: any) => GenerateAIContent(v)}
-                    loading={loading}
-                />
-
-                {/* Output Section  */}
-                <div className='col-span-2'>
-                    <OutputSection aiOutput={aiOutput} />
-                </div>
-            </div>
-
-        </div>
-    )
-}
-
 export default CreateNewContent
