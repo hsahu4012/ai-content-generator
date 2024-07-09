e1c7a149dfa1b900626fd0c745eb89338adc0112
hsahu4012 <hrudayanandasahu3@gmail.com>
2024-07-09 11:05:42 +0530

    Text Editor Added

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/react-editor';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
 
 const OutputSection = () => {
+    const editorRef: any = useRef();
+
+    const handleChange = () => {
+        if (editorRef.current && typeof editorRef.current.getInstance === 'function') {
+            console.log(editorRef.current.getInstance().getMarkdown());
+        } else {
+            console.error('Editor instance is not available or method does not exist');
+        }
+    };
+
     return (
-        <div>OutputSection</div>
-    )
+        <div className='bg-white shadow-lg border rounded-lg'>
+            <div className='flex justify-between items-center p-5'>
+                <h2 className='font-medium text-lg'>Your Result</h2>
+                <Button className='flex gap-2'><Copy className='w-4 h-4 ' /> Copy</Button>
+            </div>
+            <Editor
+                ref={editorRef}
+                initialValue="Your Result will appear here"
+                previewStyle="vertical"
+                height="600px"
+                initialEditType="wysiwyg"
+                useCommandShortcut={true}
+                onChange={handleChange}
+            />
+        </div>
+    );
 }
 
-export default OutputSection
+export default OutputSection;
